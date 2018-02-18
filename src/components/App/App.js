f21792fc9f8f0642bcbca0e47d329a2bc0550651
Nora Gully <nora.gully@gmail.com>
2018-02-17 17:50:32 -0700

    Attempt fetch to localhost back end

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,16 +4,13 @@ import { Route, NavLink, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import './App.css';
-// import { signIn } from '../../firebase'
+import { signIn } from '../../firebase'
 import { auth, provider } from '../../firebase'
 
 class App extends Component {
   constructor () {
     super(); 
     this.state = {
-      email: '', 
-      password: '', 
-      error: '',
       user: null
     }
   }
@@ -27,14 +24,21 @@ class App extends Component {
 
   signIn = () =>  {  
     return auth.signInWithPopup(provider)
-      .then((result) => {
-        const user = result.user;
-        this.setState({user})
-      })
+      .then((user) => {
+        this.setState({user});
+      }).then(this.fetchTweets())
+  }
+
+  fetchTweets = async () => {
+    const response = await fetch('/api/gettweets');
+    const data = await response.json();
+      console.log(data);
+    
+    // this.setState({data})
   }
 
   render() {
-    console.log(this.state.user)
+    console.log(this.state)
     return (
       <div className="App">
         <header className="App-header">
