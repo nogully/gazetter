d0eb1b14677c17f40affe90cbb31f3afd4f99d87
Nora Gully <nora.gully@gmail.com>
2018-02-19 17:23:25 -0700

    App fetch to back end port 3001 working but getting CORS error

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,10 +21,9 @@ export class App extends Component {
   }
 
   fetchTweets = async () => {
-    const response = await fetch('/api/gettweets');
-    const data = await response.json();
-    console.log(data);
-    this.setState({data})
+    console.log('hi i am fetchTweets')
+    const response = await fetch('http://localhost:3001/api/gettweets');
+    console.log(response);
   }
 
   render() {
@@ -42,12 +41,12 @@ export class App extends Component {
   }
 }
 export const mapStateToProps = (state) => ({
-  user: store.user;
-  tweets: store.tweets;
+  // user: store.user,
+  // tweets: store.tweets
 })
 
 export const mapDispatchToProps = () => ({
-  addUser: user => dispatch(addUser(user))
+  // addUser: user => dispatch(addUser(user))
 })
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
