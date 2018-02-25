db62e45d2d05515858b65da85c5e7f60257672d7
Nora Gully <nora.gully@gmail.com>
2018-02-25 13:22:08 -0700

    Add icon to Read link and blue color

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Article.css'
 
 const Article = ({tweet}) => {
   return (
@@ -6,8 +7,13 @@ const Article = ({tweet}) => {
       { tweet.entities.media ? <img src={tweet.entities.media[0].media_url} alt="tweet"/> : null }
       <h2>{tweet.user.name}</h2> 
       <p>{tweet.full_text}</p> 
-      { tweet.entities.urls.length ? <a href={tweet.entities.urls[0].expanded_url} target="_blank">Read</a> : null}
+      <section >
+      { tweet.entities.urls.length ? 
+        <a className="article-link" href={ tweet.entities.urls[0].expanded_url } target="_blank">
+          <i className="fas fa-newspaper"></i> Read
+        </a> : null }
       <h5><i className="fas fa-heart"></i> {tweet.favorite_count}</h5>
+      </section>
     </article>
 
   )
