cbaa6710b1859f8e056d6dba3c9ff9fb77916627
Nora Gully <nora.gully@gmail.com>
2018-02-24 20:07:29 -0700

    Add favorite count to articles

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -12,8 +12,9 @@ export class News extends Component {
       return (
         <div className="tweet" key={tweet.id}>
           { tweet.entities.media ? <img src={tweet.entities.media[0].media_url} alt="tweet"/> : null }
+          <h2>{tweet.user.name}</h2> 
           <p>{tweet.full_text}</p> 
-          <h5>– {tweet.user.name}</h5> 
+          <h5><i className="fas fa-heart"></i> {tweet.favorite_count}</h5>
         </div>
       )})
      
